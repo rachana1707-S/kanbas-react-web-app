8f5719bc589877aae00422e1354bdb0cd557c9a2
Rachana Sudhakar <rachanasudhakar17@gmail.com>
2024-11-23 18:44:23 -0500

    react hook change in session

--- src/Kanbas/Account/Session.tsx
+++ src/Kanbas/Account/Session.tsx
@@ -2,23 +2,52 @@ import * as client from "./client";
 import { useEffect, useState } from "react";
 import { setCurrentUser } from "./reducer";
 import { useDispatch } from "react-redux";
+
 export default function Session({ children }: { children: any }) {
   const [pending, setPending] = useState(true);
   const dispatch = useDispatch();
-  const fetchProfile = async () => {
-    try {
-      const currentUser = await client.profile();
-      dispatch(setCurrentUser(currentUser));
-    } catch (err: any) {
-      console.error(err);
-    }
-    setPending(false);
-  };
+
   useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const currentUser = await client.profile();
+        dispatch(setCurrentUser(currentUser));
+      } catch (err: any) {
+        console.error(err);
+      }
+      setPending(false);
+    };
     fetchProfile();
-  }, [fetchProfile]);
+  }, [dispatch]);
+
   if (!pending) {
     return children;
   }
 }
+
+
+
+// import * as client from "./client";
+// import { useEffect, useState } from "react";
+// import { setCurrentUser } from "./reducer";
+// import { useDispatch } from "react-redux";
+// export default function Session({ children }: { children: any }) {
+//   const [pending, setPending] = useState(true);
+//   const dispatch = useDispatch();
+//   const fetchProfile = async () => {
+//     try {
+//       const currentUser = await client.profile();
+//       dispatch(setCurrentUser(currentUser));
+//     } catch (err: any) {
+//       console.error(err);
+//     }
+//     setPending(false);
+//   };
+//   useEffect(() => {
+//     fetchProfile();
+//   }, [fetchProfile]);
+//   if (!pending) {
+//     return children;
+//   }
+// }
 
